292d6ed4b8db7e71cf29dab1352ef12f44e9d3d1
MarcioMengerS <gm717gem@gmail.com>
2022-10-21 17:27:22 -0300

    Inicio do projeto

---
+++ src/main/java/br/edu/restinga/ifrs/adotemeau/AdotemeauApplication.java
@@ -0,0 +1,13 @@
+package br.edu.restinga.ifrs.adotemeau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdotemeauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdotemeauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/restinga/ifrs/adotemeau/AdotemeauApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.restinga.ifrs.adotemeau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdotemeauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
