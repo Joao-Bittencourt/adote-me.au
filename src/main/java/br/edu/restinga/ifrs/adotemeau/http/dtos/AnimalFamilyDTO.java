af8d7eebcfa98e7895a0dc2f71aaa34930b6495c
joao bittencourt <joaogsbittencourt@gmail.com>
2022-12-10 22:22:01 -0300

    Task-42: Criar CRUD de família do animal.

--- src/main/java/br/edu/restinga/ifrs/adotemeau/http/dtos/AnimalFamilyDTO.java
+++ src/main/java/br/edu/restinga/ifrs/adotemeau/http/dtos/AnimalFamilyDTO.java
@@ -1,12 +1,11 @@
 package br.edu.restinga.ifrs.adotemeau.http.dtos;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-
 import br.edu.restinga.ifrs.adotemeau.models.AnimalFamily;
+
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.validation.constraints.NotBlank;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,7 +20,6 @@ public class AnimalFamilyDTO {
     @NotBlank(message = "O campo não pode ser nulo, vazio ou em branco")
     private String type;
 
-//    @NotNull(message = "O campo não pode ser nulo, vazio ou em branco")
     private boolean active;
 
     public AnimalFamilyDTO(AnimalFamily animalFamily) {
